84bf137f3e6f458704d5f8e3a65c6cee84107eba
Mahezer Carvalho de Melo <mahezer.melo@luizalabs.com>
2018-10-28 09:06:02 -0300

    Ajustar tela de detalhes

--- src/views/Detalhe/Detalhe.jsx
+++ src/views/Detalhe/Detalhe.jsx
@@ -12,88 +12,98 @@ import {
 // react plugin used to create charts
 import { Line, Pie } from "react-chartjs-2";
 // function that returns a color based on an interval of numbers
-
 import Stats from "components/Stats/Stats.jsx";
 
+import axios from "axios";
 import {
   dashboard24HoursPerformanceChart,
   dashboardEmailStatisticsChart,
   dashboardNASDAQChart
 } from "variables/charts.jsx";
 
-import { Redirect } from 'react-router';
-import { Router, Route, Switch } from "react-router-dom";
-import {Link} from 'react-router-dom'
+import { Redirect } from "react-router";
+import { Router, Route, Switch, withRouter } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-import Cartao from '../card_rotas.js'
-
-
+import Cartao from "../card_rotas.js";
 
 class Dashboard extends React.Component {
   constructor(props) {
-    super(props)
-    this.clickCard1 = this.clickCard1.bind(this)
+    super(props);
     this.state = {
-    toCard: false
-    }
-  }
-  
-  clickCard1(){
-    this.setState({
-     toCard: true 
-    })
+      vaga: {
+        titulo: "",
+        descricaovaga: ""
+      }
+    };
+    this.handleClick = this.handleClick.bind(this);
   }
 
+  componentDidMount() {
+    const id = this.props.location.pathname.split("/")[2];
+    axios.get("http://10.0.8.72:3001/vagas/" + id).then(response => {
+      this.setState({
+        vaga: response.data[0]
+      });
+    });
+  }
+
+  handleClick() {
+    const id = this.props.location.pathname.split("/")[2];
+    axios.post("http://10.0.8.72:3001/candidato", {
+      id_voluntario: 1,
+      id_vaga: id
+    });
+  }
 
   render() {
-
-
     return (
       <div className="content">
-        <Col xs={12} sm={6} md={6} lg={3}> 
-              <Link to="/detalhe" className="nav-link btn-magnify">
-                <Card className="card-stats">
-                  <CardBody>
-                    <Row>
-                        <div className="numbers">
-                          <CardTitle tag="p" className="text-center">Ação: Palestra</CardTitle>
-                          <br></br>
-                          <p className="text-justify pr-4 pl-4">Neste evento será ministrada uma palaestra sobre a prevenção do suicídio</p>
-                        </div>
-                    </Row>
-                  </CardBody>
-                </Card>
-              </Link>
-            </Col>
+        <Col xs={12} sm={6} md={6} lg={3}>
+          <Link to="/detalhe" className="nav-link btn-magnify">
+            <Card className="card-stats">
+              <CardBody>
+                <Row>
+                  <div className="numbers">
+                    <CardTitle tag="p" className="text-center">
+                      {this.state.vaga.titulo}
+                    </CardTitle>
+                    <br />
+                  </div>
+                </Row>
+              </CardBody>
+            </Card>
+          </Link>
+        </Col>
 
-            <Col xs={12} sm={6} md={6} lg={3}> 
-              <Link to="/detalhe" className="nav-link btn-magnify">
-                <Card className="card-stats">
-                  <CardBody>
-                    <Row>
-                        <div className="numbers">
-                          <CardTitle tag="p" className="text-center">Precisamos de:</CardTitle>
-                          <br></br>
-                          <p className="text-left pl-4">5 - Panfletores</p>
-                          <br></br>
-                          <p className="text-left pl-4">Técnicos:</p>
-                          <p className="text-left pl-4">1 - Psicólogo</p>
-                          <p className="text-left pl-4">2 - Assistentes Sociais</p>
-                        </div>
-                    </Row>
-                  </CardBody>
-                </Card>
-              </Link>
-            </Col>
+        <Col xs={12} sm={6} md={6} lg={3}>
+          <Link to="/detalhe" className="nav-link btn-magnify">
+            <Card className="card-stats">
+              <CardBody>
+                <Row>
+                  <div className="numbers">
+                    <p className="text-justify pr-4 pl-4">
+                      {this.state.vaga.descricaovaga}
+                    </p>
+                  </div>
+                </Row>
+              </CardBody>
+            </Card>
+          </Link>
+        </Col>
 
-            <Row>
-              <div className="update ml-auto mr-auto">
-                <Button color="primary" round>Quero me Candidatar!</Button>
-              </div>
-            </Row>
+        <Row>
+          <div className="update ml-auto mr-auto">
+            <Link to="/dashboard" className="nav-link btn-magnify">
+              <Button color="primary" round onClick={() => this.handleClick()}>
+                Quero me Candidatar!
+              </Button>
+            </Link>
+          </div>
+        </Row>
       </div>
     );
   }
 }
 
-export default Dashboard;
+export default withRouter(Dashboard);
--- src/views/login/login.js
+++ src/views/login/login.js
@@ -10,67 +10,100 @@ import {
   Button
 } from "reactstrap";
 // react plugin used to create charts
-import { Line, Pie } from "react-chartjs-2";
-// function that returns a color based on an interval of numbers
-
-import Stats from "components/Stats/Stats.jsx";
-
-import {
-  dashboard24HoursPerformanceChart,
-  dashboardEmailStatisticsChart,
-  dashboardNASDAQChart
-} from "variables/charts.jsx";
-
-import { Redirect } from 'react-router';
+import { Redirect } from "react-router";
 import { Router, Route, Switch } from "react-router-dom";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 import FormInputs from "components/FormInputs/FormInputs.jsx";
-const Login = (props) =>{
-	return(
-		<div>
+import axios from "axios";
 
-            <Col xs={12} sm={6} md={6} lg={3}> 
-                <Card className="card-stats login-card">
-                  <CardBody>
-                    <Row>
-                        <div className="numbers">
-                          <CardTitle tag="p" className="text-center">Login</CardTitle>
-                          <br></br>
-                          <form>
-			                  <FormInputs
-			                    ncols={["col-md-4", "col-md-4"]}
-			                    proprieties={[
-			                      {
-			                        inputProps: {
-			                          type: "text",
-			                          placeholder: "Login",
-			                          className: "login-input ml-auto mr-auto"
-			                        }
-			                      },
-			                      {
-			                        inputProps: {
-			                          type: "text",
-			                          placeholder: "Senha",
-			                          className: "login-input ml-auto mr-auto" 
-			                        }
-			                      }
-			                    ]}
-			                  />
-			              </form>
-                        </div>
-                    </Row>
-                  </CardBody>
-                </Card>
-            </Col>
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
 
-            <Row>
-              <div className="update ml-auto mr-auto">
-                <Button color="primary" round>Acessar</Button>
-              </div>
-            </Row>
+    this.state = {
+      login: "",
+      senha: ""
+    };
 
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    const { login, senha } = this.state;
+    axios
+      .post("http://10.0.8.72:3001/login", {
+        email: login,
+        senha,
+        tipo: "2"
+      })
+      .then(response => {
+        console.log(response);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  handleChange(event) {
+    const { target } = event;
+    this.setState({ [target.name]: target.value });
+  }
+
+  render() {
+    return (
+      <div>
+        <Col xs={12} sm={6} md={6} lg={3}>
+          <Card className="card-stats login-card">
+            <CardBody>
+              <Row>
+                <div className="numbers">
+                  <CardTitle tag="p" className="text-center">
+                    Login
+                  </CardTitle>
+                  <br />
+                  <form>
+                    <FormInputs
+                      ncols={["col-md-4", "col-md-4"]}
+                      proprieties={[
+                        {
+                          inputProps: {
+                            type: "text",
+                            placeholder: "Login",
+                            className: "login-input ml-auto mr-auto",
+                            name: "login",
+                            onChange: this.handleChange,
+                            value: this.state.login
+                          }
+                        },
+                        {
+                          inputProps: {
+                            type: "text",
+                            name: "senha",
+                            placeholder: "Senha",
+                            className: "login-input ml-auto mr-auto",
+                            onChange: this.handleChange,
+                            value: this.state.senha
+                          }
+                        }
+                      ]}
+                    />
+                  </form>
+                </div>
+              </Row>
+            </CardBody>
+          </Card>
+        </Col>
+        <Row>
+          <div className="update ml-auto mr-auto">
+            <Button color="primary" round onClick={() => this.handleClick()}>
+              Acessar
+            </Button>
+          </div>
+        </Row>
       </div>
-		)
+    );
+  }
 }
 
 export default Login;
